b4d36934e2b3dd1a8a6b57b5dd27fe196f94d81f
Tran Quoc Ung <ungtq0805@gmail.com>
2018-03-20 08:51:28 +0700

    Update page master company info

--- jcart-master/jcart-admin/src/main/java/com/cts/jcart/admin/web/controllers/MasterController.java
+++ jcart-master/jcart-admin/src/main/java/com/cts/jcart/admin/web/controllers/MasterController.java
@@ -6,7 +6,11 @@ package com.cts.jcart.admin.web.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cts.jcart.admin.web.models.MstCompanyInfoForm;
 import com.cts.jcart.catalog.MasterCommonService;
@@ -28,6 +32,12 @@ public class MasterController extends JCartAdminBaseController{
 		return "Company Information";
 	}
 	
+	/**
+	 * @author ungtq
+	 * edit company info to Form
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping("/edit/companyInfo")
 	public String editCompanyInfoHome(Model model){
 		//GET COMPANY INFO
@@ -39,6 +49,35 @@ public class MasterController extends JCartAdminBaseController{
 			model.addAttribute("mstCompanyInfo", MstCompanyInfoForm.fromMstCommon(mstCommon));
 		}
 		
+		return viewPrefix + "company_info";
+	}
+	
+	/**
+	 * @author ungtq
+	 * @param mstCompanyInfoForm
+	 * @param result
+	 * @param model
+	 * @param redirectAttributes
+	 * @return Update company
+	 */
+	@RequestMapping(value="/update/companyInfo", method=RequestMethod.POST)
+	public String updateCompanyInfo(
+			@ModelAttribute("mstCompanyInfo") MstCompanyInfoForm mstCompanyInfoForm, 
+			BindingResult result, 
+			Model model, 
+			RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()){
+			return viewPrefix + "company_info";
+		}
+		
+		
+		//convert to entity
+		MstCommon mstCompanyInfo = mstCompanyInfoForm.toMstCommon();
+		
+		//update Master Company Info
+		mstCommonService.updateMstCompanyInfo(mstCompanyInfo);
+		
+		redirectAttributes.addFlashAttribute("info", "Company Info updates successfully");
 		return viewPrefix + "company_info";
 	}
 }
--- jcart-master/jcart-core/src/main/java/com/cts/jcart/catalog/MasterCommonService.java
+++ jcart-master/jcart-core/src/main/java/com/cts/jcart/catalog/MasterCommonService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cts.jcart.JCartException;
+import com.cts.jcart.constant.MstCmnConst;
 import com.cts.jcart.entities.MstCommon;
 
 /**
@@ -27,5 +29,28 @@ public class MasterCommonService {
 	 */
 	public MstCommon getMstCommonByCommonNoAndClassNo(String commonNo, String classNo) {
 		return masterCommonRepository.getMstCommonByCommonNoAndClassNo(commonNo, classNo);
+	}
+	
+	/**
+	 * @author ungtq
+	 * @param mstCompanyInfo
+	 * @return MstCommon
+	 */
+	public MstCommon updateMstCompanyInfo(MstCommon mstCompanyInfo) {
+		MstCommon mstLatestCompanyInfo = getMstCommonByCommonNoAndClassNo(
+				MstCmnConst.MST_COMPANY_INFO, 
+				MstCmnConst.MST_COMPANY_INFO_RELATED);
+		if(mstLatestCompanyInfo == null){
+			throw new JCartException("mstLatestCompanyInfo doesn't exist");
+		}
+		
+		mstLatestCompanyInfo.setCharData1(mstCompanyInfo.getCharData1());
+		mstLatestCompanyInfo.setCharData2(mstCompanyInfo.getCharData2());
+		mstLatestCompanyInfo.setCharData3(mstCompanyInfo.getCharData3());
+		mstLatestCompanyInfo.setCharData4(mstCompanyInfo.getCharData4());
+		mstLatestCompanyInfo.setCharData5(mstCompanyInfo.getCharData5());
+		mstLatestCompanyInfo.setCharData6(mstCompanyInfo.getCharData6());
+		
+		return masterCommonRepository.save(mstLatestCompanyInfo);
 	}
 }
