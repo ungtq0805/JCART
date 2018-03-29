d54ff4883b2ffac6f480cd5df22d3bc3c56cffbb
Tran Quoc Ung <ungtq0805@gmail.com>
2018-03-29 15:05:02 +0700

    maintenance inflow and outflow

---
+++ jcart-master/jcart-admin/src/main/java/com/cts/jcart/admin/web/validators/OutflowFormValidator.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.cts.jcart.admin.web.validators;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.cts.jcart.catalog.CatalogService;
+import com.cts.jcart.entities.Category;
+
+/**
+ * @author ungtq
+ *
+ */
+@Component
+public class OutflowFormValidator implements Validator
+{
+	@Autowired protected MessageSource messageSource;
+	@Autowired protected CatalogService catalogService;
+	
+	@Override
+	public boolean supports(Class<?> clazz){
+		return Category.class.isAssignableFrom(clazz);
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors){
+		
+	}
+}
--- jcart-master/jcart-admin/src/main/java/com/cts/jcart/admin/wh/controllers/WhOutflowsController.java
+++ jcart-master/jcart-admin/src/main/java/com/cts/jcart/admin/wh/controllers/WhOutflowsController.java
@@ -1,13 +1,25 @@
 package com.cts.jcart.admin.wh.controllers;
 
+import java.util.Calendar;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.cts.jcart.admin.web.models.WhOutflowForm;
+import com.cts.jcart.admin.web.validators.OutflowFormValidator;
+import com.cts.jcart.customers.CustomerService;
+import com.cts.jcart.entities.Customer;
+import com.cts.jcart.wh.entities.WhInflow;
 import com.cts.jcart.wh.entities.WhOutflow;
 import com.cts.jcart.wh.impl.WhOutflowsData;
 
@@ -23,8 +35,23 @@ public class WhOutflowsController extends WhAbstractController {
     
 	private static final String viewPrefix = "wh/";
 	
+	public static final String NOSUCHAMOUNT = "There is no such amount in the chosen warehouse";
+	
     @Autowired
     WhOutflowsData outflowsData;
+    
+    @Autowired 
+    private OutflowFormValidator outflowFormValidator;
+    
+    @Autowired 
+	private CustomerService customerService;
+    
+    WhInflow inflow;
+    
+    @ModelAttribute("customers")
+	public List<Customer> categoriesList(){
+		return customerService.getAllCustomers();
+	}
     
     /**
      * Gets outflows and renders them
@@ -37,5 +64,69 @@ public class WhOutflowsController extends WhAbstractController {
         List<WhOutflow> outflows = outflowsData.get();
         model.addAttribute("outflows", outflows);
         return viewPrefix + "outflows";
+    }
+    
+    /**
+     * edit warehouse form with mode edit 
+     * @param id
+     * @param model
+     * @return
+     */
+    @RequestMapping(value="/wh/edit/outflow/{id}", method=RequestMethod.GET)
+	public String editOutflowForm(@PathVariable Integer id, Model model) {
+    	WhOutflow whOutflow = outflowsData.get((long)id);
+    	WhOutflowForm whOutflowForm = WhOutflowForm.fromWhOutflow(whOutflow);
+		model.addAttribute("outflow", whOutflowForm);
+		
+		//get inflow
+		inflow = whOutflow.getInflow();
+		model.addAttribute("product_name", inflow.getProduct().getName());
+		
+		return viewPrefix + "stock_buy_edit";
+	}
+    
+    /**
+     * Handles the submit action for a update warehouse
+     * @param warehouse object with all product input information
+     * @param result validation information about the current action
+     * @return name which will be resolved into the jsp page using
+     * apache tiles configuration in the warehouses.xml file
+     */
+    @RequestMapping(value = "/wh/outflow/update/{id}", method = RequestMethod.POST)
+    public String updateInflow(
+    		@Valid @ModelAttribute("outflow") WhOutflowForm outflowForm,
+    		BindingResult result,
+    		ModelMap model) {
+    	outflowFormValidator.validate(outflowForm, result);
+        if (result.hasErrors()) {
+            return viewPrefix + "stock_buy_edit";
+        } else {
+        	
+        	//check quantities
+        	if (outflowForm.getAmount() > inflow.getLeft()) {
+        		result.rejectValue("amount", "", NOSUCHAMOUNT);
+        		return viewPrefix + "stock_buy";
+        	}
+        	
+        	WhOutflow persistedInflow = outflowForm.toWhOutflow();
+        	persistedInflow.setLastUpdDate(Calendar.getInstance().getTime());
+        	outflowsData.update(persistedInflow);
+            return "redirect:/wh/outflows";
+        }
+    }
+    
+    /**
+     * Handles the submit action for a delete warehouse
+     * @param warehouse object with all product input information
+     * @param result validation information about the current action
+     * @return name which will be resolved into the jsp page using
+     * apache tiles configuration in the warehouses.xml file
+     */
+    @RequestMapping(value = "/wh/outflow/delete/{id}", method = RequestMethod.GET)
+    public String delInflowById(
+    		@PathVariable Integer id,
+    		Model model) {
+    	outflowsData.removeById((long)id);
+        return "redirect:/wh/outflows";
     }
 }
