9b653813c08c37c26d1df9c2fb4fbc90ade9bb69
Tran Quoc Ung <ungtq0805@gmail.com>
2018-04-09 14:00:35 +0700

    update home.js

--- jcart-master/jcart-admin/src/main/resources/static/assets/dist/js/home.js
+++ jcart-master/jcart-admin/src/main/resources/static/assets/dist/js/home.js
@@ -1,18 +1,13 @@
-$(document).ready(function() {
-	//$('#rdCurrentDay').attr('checked', 'checked');
-	
-	$("#rdCurrentMonth").click(function(){ 
-	    $('#rdCurrentMonth').prop("checked", true);// .attr("checked", "checked");
-	    
-	    var url = '/home/by_month';
-		window.location = url;
-	});
-	
-	$("#rdCurrentDay").click(function(){ 
-	    $('#rdCurrentDay').attr("checked", "checked");
-	    
-	    var url = '/home/by_day';
-		window.location = url;
-	});
-});
+function onClickRdCurrentMonth() {
+	$('#rdCurrentMonth').prop("checked", true);
+    
+    var url = '/home/by_month';
+	window.location = url;
+}
 
+function onClickRdCurrentDay() {
+	$('#rdCurrentDay').attr("checked", "checked");
+    
+    var url = '/home/by_day';
+	window.location = url;
+}
