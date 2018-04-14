f02de7075e817f6a6d7e4661eb15fbd2562d5331
Tran Quoc Ung <ungtq0805@gmail.com>
2018-04-14 16:10:16 +0700

    update

--- jcart-master/jcart-admin/src/main/java/com/cts/jcart/admin/web/utils/WebUtils.java
+++ jcart-master/jcart-admin/src/main/java/com/cts/jcart/admin/web/utils/WebUtils.java
@@ -5,24 +5,18 @@ package com.cts.jcart.admin.web.utils;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-
 /**
  * @author ungtq
  *
  */
 public class WebUtils{
 	
-	@Autowired protected MessageSource messageSource;
-	
 	private WebUtils(){
-		
 	}
 	
 	public static final String IMAGES_PREFIX = "/products/images/";
-	public static final String IMAGES_DIR =  "D:/jcart/products/";
-	public static final String IMAGES_USER_DIR =  "D:/jcart/user/";
+	public static final String IMAGES_USERS_DIR = "img.user.path.dir";
+	public static final String IMAGES_PRODUCTS_DIR = "img.product.path.dir";
 	
 	public static String getURLWithContextPath(HttpServletRequest request){
 		return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
