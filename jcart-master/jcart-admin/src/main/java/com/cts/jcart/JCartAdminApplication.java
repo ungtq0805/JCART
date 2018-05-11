cb6fc89a7bbb611aa3db54268f60e3355d807551
Tran Quoc Ung <ungtq0805@gmail.com>
2018-05-11 17:02:25 +0700

    config with war file

--- jcart-master/jcart-admin/src/main/java/com/cts/jcart/JCartAdminApplication.java
+++ jcart-master/jcart-admin/src/main/java/com/cts/jcart/JCartAdminApplication.java
@@ -5,13 +5,20 @@ package com.cts.jcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * @author ungtq
  *
  */
 @SpringBootApplication
-public class JCartAdminApplication{
+public class JCartAdminApplication extends SpringBootServletInitializer{
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JCartAdminApplication.class);
+    }
 
 	public static void main(String[] args){
 		SpringApplication.run(JCartAdminApplication.class, args);
